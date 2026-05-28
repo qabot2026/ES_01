ceddeb00b82f253ad362764d9300130f86cdb07b
qabot2026 <qabot2026@users.noreply.github.com>
2026-05-29 00:25:16 +0530

    Add shaking wave motion to strip hand pop animation

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var QA_ASSET_VERSION = '20260529-strip-hand-pop-inline';
+  var QA_ASSET_VERSION = '20260529-strip-hand-shake';
 
   var script = document.currentScript;
   var base = '';
