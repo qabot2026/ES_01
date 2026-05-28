2ca0144ab0973f393a8a98ad8dbe25a0fe2e37e6
qabot2026 <qabot2026@users.noreply.github.com>
2026-05-28 22:27:26 +0530

    Simplify company.config.js: remove cost markers and extra comments

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var QA_ASSET_VERSION = '20260529-layout-desk-right-mob-left';
+  var QA_ASSET_VERSION = '20260529-config-cleanup';
 
   var script = document.currentScript;
   var base = '';
