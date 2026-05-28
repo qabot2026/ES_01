1486d1faaadb2d9a23427eb530e1b7aa5e73fcca
qabot2026 <qabot2026@users.noreply.github.com>
2026-05-29 01:03:44 +0530

    Offset strip hand wave 15px left and 15px up during pop

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var QA_ASSET_VERSION = '20260529-hand-pop-in-place';
+  var QA_ASSET_VERSION = '20260529-hand-pop-offset';
 
   var script = document.currentScript;
   var base = '';
