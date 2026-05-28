bead0af4993977f3985b595199f8d410f5ae8190
qabot2026 <qabot2026@users.noreply.github.com>
2026-05-29 00:52:14 +0530

    Size chips to label text width in card carousel and all button types

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var QA_ASSET_VERSION = '20260529-hand-pop-right';
+  var QA_ASSET_VERSION = '20260529-chip-text-width';
 
   var script = document.currentScript;
   var base = '';
