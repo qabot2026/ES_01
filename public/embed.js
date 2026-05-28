7236739df651893a068ed9278aed55100f1eec8e
qabot2026 <qabot2026@users.noreply.github.com>
2026-05-29 00:19:16 +0530

    Bump embed cache version after reverting wave popup

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  var QA_ASSET_VERSION = '20260529-panel-height-extra-fix';
+  var QA_ASSET_VERSION = '20260529-revert-wave-popup';
 
   var script = document.currentScript;
   var base = '';
